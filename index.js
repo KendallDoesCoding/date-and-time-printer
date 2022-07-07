21035d862a1bc1ba76b3ffae0a50eb0566fab815
Kendall Pinto <kendalldoescoding@gmail.com>
2022-07-07 21:51:24 +0530

    update date and time in real time

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var datetime = new Date();
 console.log(datetime);
 document.getElementById("time").textContent = datetime; //it will print on html page
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+  setInterval(refreshTime, 1000);
